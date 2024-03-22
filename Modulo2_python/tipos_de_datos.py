cf1e9c489f69e74d1daecd0e4b23ecb1536bcbcf
ELTOBI360 <mneyraarzapalo@gmail.com>
2024-03-21 21:29:21 -0500

    Datos de python
    ejemplos de tipos de datos en python

---
+++ Modulo2_python/tipos_de_datos.py
@@ -0,0 +1,26 @@
+edad=22 
+nombre="Mauricio"
+#Funcion print inprime el valor en el terminal
+
+print(edad)
+print(nombre)
+
+#imprimir conviando texto con el valor
+
+print("Mi edad es: ", edad)
+print("Mi nombre es: ", nombre)
+
+edad =20
+nombre = "Jorge"
+
+print("Mi nombre despues de modificarla: ", edad)
+print("Mi nombre despues de modificarla: ", nombre)
+
+
+#_________________
+
+a=10
+
+a=10+1
+
+print("El valor de a es :", a)
